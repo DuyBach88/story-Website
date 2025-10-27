0835f1c86ad51acd90b3cdfe20af078d065af578
bachdnd <bachbeo200003@gmail.com>
2025-10-27 08:13:48 +0700

    update code

--- src/sections/PhaseAnalysisBlock1.jsx
+++ src/sections/PhaseAnalysisBlock1.jsx
@@ -1,8 +1,6 @@
 // src/sections/PhaseAnalysisBlock1.jsx
-
 import React from "react";
 import { motion } from "framer-motion";
-
 export default function PhaseAnalysisBlock1({ onNext }) {
   return (
     <motion.section
@@ -68,7 +66,7 @@ export default function PhaseAnalysisBlock1({ onNext }) {
           className="rounded-xl overflow-hidden shadow-2xl"
         >
           <img
-            src="../../public/assets/analyse_b52.jpg"
+            src="/assets/analyse_b52.jpg"
             alt="Chủ tịch Hồ Chí Minh đang phân tích chiến lược"
             className="w-full h-full object-cover"
           />
